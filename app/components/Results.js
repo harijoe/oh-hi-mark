a1bca0e0500f93889331753894cacbf24df82092
Julien Vallini <vallinij@gmail.com>
2016-10-14 20:19:46 +0200

    Add contribution link

--- app/components/Results.js
+++ app/components/Results.js
@@ -24,41 +24,52 @@ function Results(props) {
   };
 
   return (
-    <Table>
-      <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-        <TableRow>
-          <TableHeaderColumn style={{ width: '7%' }}>Logo</TableHeaderColumn>
-          <TableHeaderColumn style={{ width: '45%' }}>Title</TableHeaderColumn>
-          <TableHeaderColumn style={{ width: '25%' }}>Url</TableHeaderColumn>
-          <TableHeaderColumn style={{ width: '23%' }}>Added</TableHeaderColumn>
-        </TableRow>
-      </TableHeader>
-      <TableBody displayRowCheckbox={false} deselectOnClickaway={false}>
-        {props.results.map((result, index) => (
-          <TableRow
-            key={result.id}
-            selected={result.id === props.selectedId}
-            onMouseMove={onRowOver(index)}
-            onClick={onClick}
-            selectable={false}
-            className={css(styles.tableRow)}
-          >
-            <TableRowColumn style={{ width: '7%' }}>
-              <img alt={result.title} src={result.favicon} />
-            </TableRowColumn>
-            <TableRowColumn style={{ width: '45%' }}>
-              {result.title}
-            </TableRowColumn>
-            <TableRowColumn style={{ width: '25%' }}>
-              {cleanUrl(result.url).split('/')[0]}
-            </TableRowColumn>
-            <TableRowColumn style={{ width: '23%' }}>
-              {result.date != null && moment(result.date).fromNow()}
-            </TableRowColumn>
+    <div>
+      <Table>
+        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+          <TableRow>
+            <TableHeaderColumn style={{ width: '7%' }}>Logo</TableHeaderColumn>
+            <TableHeaderColumn style={{ width: '45%' }}>Title</TableHeaderColumn>
+            <TableHeaderColumn style={{ width: '25%' }}>Url</TableHeaderColumn>
+            <TableHeaderColumn style={{ width: '23%' }}>Added</TableHeaderColumn>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false} deselectOnClickaway={false}>
+          {props.results.map((result, index) => (
+            <TableRow
+              key={result.id}
+              selected={result.id === props.selectedId}
+              onMouseMove={onRowOver(index)}
+              onClick={onClick}
+              selectable={false}
+              className={css(styles.tableRow)}
+            >
+              <TableRowColumn style={{ width: '7%' }}>
+                <img alt={result.title} src={result.favicon} />
+              </TableRowColumn>
+              <TableRowColumn style={{ width: '45%' }}>
+                {result.title}
+              </TableRowColumn>
+              <TableRowColumn style={{ width: '25%' }}>
+                {cleanUrl(result.url).split('/')[0]}
+              </TableRowColumn>
+              <TableRowColumn style={{ width: '23%' }}>
+                {result.date != null && moment(result.date).fromNow()}
+              </TableRowColumn>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {/* TODO DEBUG IT */}
+      <p className={css(styles.productPain)}>
+        <a
+          className={css(styles.productPainLink)}
+          href="https://productpains.com/product/oh-hi-mark"
+        >
+          Something is missing ? Vote for the next feature!
+        </a>
+      </p>
+    </div>
   );
 }
 
@@ -71,6 +82,18 @@ const styles = StyleSheet.create({
     color: 'grey',
     height: 60,
     lineHeight: '60px',
+  },
+  productPain: {
+    textAlign: 'right',
+    marginRight: 10,
+  },
+  productPainLink: {
+    color: 'grey',
+    fontStyle: 'italic',
+    textDecoration: 'inherit',
+    ':hover': {
+      textDecoration: 'underline',
+    }
   }
 });
 
