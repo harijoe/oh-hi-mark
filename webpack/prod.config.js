94962d4c6daa5f32ddf9268445d36247cf01986f
Julien Vallini <vallinij@gmail.com>
2016-11-14 19:40:20 +0700

    Update manifest json

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -30,11 +30,11 @@ module.exports = {
     })
   ],
   resolve: {
-    extensions: ['', '.js', '.json']
+    extensions: ['', '.js', '.jsx', '.json']
   },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       loader: 'babel',
       exclude: /node_modules/
     }, {
